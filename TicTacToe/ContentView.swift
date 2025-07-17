9c2efc1ab1b950ccf19fdcbef1f2b9ec00fdafbe
EhsanMousaviMsl <ehsanmousavi7472@yahoo.com>
2025-07-17 19:04:46 +0300

    project updated

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -1,73 +1,79 @@
 import SwiftUI
 
-struct ContentView: View
-{
-    @StateObject var gameState = GameState()
+struct ContentView: View {
+    @StateObject var gameState: GameState
     
-    var body: some View
-    {
+    var body: some View {
         let borderSize = CGFloat(5)
         
-        Text(gameState.turnText())
-            .font(.title)
-            .bold()
-            .padding()
-        Spacer()
-        
-        Text(String(format: "Crosses: %d", gameState.crossesScore))
-            .font(.title)
-            .bold()
-            .padding()
-        
-        VStack(spacing: borderSize)
-        {
-            ForEach(0...2, id: \.self)
-            {
-                row in
-                HStack(spacing: borderSize)
-                {
-                    ForEach(0...2, id: \.self)
-                    {
-                        column in
-                        
-                        let cell = gameState.board[row][column]
-                        
-                        Text(cell.displayTile())
-                            .font(.system(size: 60))
-                            .foregroundColor(cell.tileColor())
-                            .bold()
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .aspectRatio(1, contentMode: .fit)
-                            .background(Color.white)
-                            .onTapGesture {
-                                gameState.placeTile(row, column)
-                            }
+        VStack {
+            HStack {
+                Text(gameState.gameMode == .singlePlayer ? "Single Player" : "Multiplayer")
+                    .font(.headline)
+                
+                Spacer()
+                
+                Button("Reset Game") {
+                    gameState.resetGame()
+                }
+            }
+            .padding(.horizontal)
+            
+            Text(gameState.turnText())
+                .font(.title)
+                .bold()
+                .padding()
+            
+            Spacer()
+            
+            Text(String(format: "Crosses: %d", gameState.crossesScore))
+                .font(.title)
+                .bold()
+                .padding()
+            
+            VStack(spacing: borderSize) {
+                ForEach(0...2, id: \.self) { row in
+                    HStack(spacing: borderSize) {
+                        ForEach(0...2, id: \.self) { column in
+                            let cell = gameState.board[row][column]
+                            
+                            Text(cell.displayTile())
+                                .font(.system(size: 60))
+                                .foregroundColor(cell.tileColor())
+                                .bold()
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .aspectRatio(1, contentMode: .fit)
+                                .background(Color.white)
+                                .onTapGesture {
+                                    // Only allow human player to tap in single player mode
+                                    if gameState.gameMode == .multiPlayer ||
+                                       (gameState.gameMode == .singlePlayer && gameState.turn == .Cross) {
+                                        gameState.placeTile(row, column)
+                                    }
+                                }
+                        }
                     }
                 }
-                
             }
-        }
-        .background(Color.black)
-        .padding()
-        .alert(isPresented: $gameState.showAlert)
-        {
-            Alert(
-                title: Text(gameState.alertMessage),
-                dismissButton: .default(Text("Okay"))
-                {
-                    gameState.resetBoard()
-                }
-            )
-        }
-        
-        Text(String(format: "Noughts: %d", gameState.noughtsScore))
-            .font(.title)
-            .bold()
+            .background(Color.black)
             .padding()
-        Spacer()
+            .alert(isPresented: $gameState.showAlert) {
+                Alert(
+                    title: Text(gameState.alertMessage),
+                    dismissButton: .default(Text("Okay")) {
+                        gameState.resetBoard()
+                    }
+                )
+            }
+            
+            Text(String(format: "Noughts: %d", gameState.noughtsScore))
+                .font(.title)
+                .bold()
+                .padding()
+            
+            Spacer()
+        }
     }
 }
 
-#Preview {
-    ContentView()
-}
+
--- TicTacToe/GameState.swift
+++ TicTacToe/GameState.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+enum GameMode {
+    case singlePlayer
+    case multiPlayer
+}
+
 class GameState: ObservableObject
 {
     @Published var board = [[Cell]]()
@@ -8,6 +13,8 @@ class GameState: ObservableObject
     @Published var crossesScore = 0
     @Published var showAlert = false
     @Published var alertMessage = "Draw"
+    @Published var gameMode: GameMode = .multiPlayer
+    
     
     
     
@@ -21,41 +28,53 @@ class GameState: ObservableObject
         return turn == Tile.Cross ? "Turn: X" : "Turn: O"
     }
     
-    func placeTile(_ row: Int,_ column: Int)
-    {
-        if(board[row][column].tile != Tile.Empty)
-        {
+    func placeTile(_ row: Int, _ column: Int) {
+        if board[row][column].tile != Tile.Empty {
             return
         }
         
         board[row][column].tile = turn == Tile.Cross ? Tile.Cross : Tile.Nought
         
-        
-        if(checkForVictory())
-        {
-            if(turn == Tile.Cross)
-            {
+        if checkForVictory() {
+            if turn == Tile.Cross {
                 crossesScore += 1
-            }
-            else
-            {
+            } else {
                 noughtsScore += 1
             }
             let winner = turn == Tile.Cross ? "Crosses" : "Noughts"
             alertMessage = winner + " Win!"
             showAlert = true
-        }
-        else
-        {
-            turn = turn == Tile.Cross ? Tile.Nought : Tile.Cross
-        }
-        
-        if(checkForDraw())
-        {
+        } else if checkForDraw() {
             alertMessage = "Draw"
             showAlert = true
+        } else {
+            turn = turn == Tile.Cross ? Tile.Nought : Tile.Cross
+            
+
+            if gameMode == .singlePlayer && turn == Tile.Nought {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.makeAIMove()
+                }
+            }
         }
     }
+    
+    private func makeAIMove() {
+            var availableMoves = [(Int, Int)]()
+            
+            for (rowIndex, row) in board.enumerated() {
+                for (colIndex, cell) in row.enumerated() {
+                    if cell.tile == Tile.Empty {
+                        availableMoves.append((rowIndex, colIndex))
+                    }
+                }
+            }
+            
+            if let randomMove = availableMoves.randomElement() {
+                placeTile(randomMove.0, randomMove.1)
+            }
+        }
+
     
     func checkForDraw() -> Bool
     {
@@ -137,4 +156,11 @@ class GameState: ObservableObject
         }
         board = newBoard
     }
+    
+    func resetGame() {
+        resetBoard()
+        noughtsScore = 0
+        crossesScore = 0
+    }
+    
 }
